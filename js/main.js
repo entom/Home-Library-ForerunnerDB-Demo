4f2b5e0175afb5d833cd3b298a9a6fc931d4d768
entom <tomasz@entom.info>
2019-02-10 20:09:17 +0100

    saving data from form into local database

--- js/main.js
+++ js/main.js
@@ -2,12 +2,34 @@ const collectionBooks = 'books'
 
 let fdb = new ForerunnerDB()
 let db = fdb.db("home_library")
+db.persist.driver("WebSQL")
+
 let booksContainer = document.getElementById('BooksContainer')
 
-loadCollection()
+document.getElementById('AddBookSubmitButton').addEventListener('click', () => {
+  let bookTitle = document.getElementById('bookTitle')
+  let bookAuthor = document.getElementById('bookAuthor')
+  let bookYear = document.getElementById('bookYear')
+  let bookPages = document.getElementById('bookPages')
+
+  loadCollection().then(() => {
+    let collection = db.collection(collectionBooks)
+    let bookObject = {
+      title: bookTitle,
+      author: bookAuthor,
+      year: bookYear,
+      pages: bookPages
+    }
+    collection.insert(bookObject)
+    collection.save()
+  })
+})
 
 function loadCollection () {
-  db.collection(collectionBooks).load(() => {
-    console.log('loaded')
+  return new Promise(resolve => {
+    db.collection(collectionBooks).load(() => {
+      console.log('loaded')
+      resolve()
+    })
   })
 }
